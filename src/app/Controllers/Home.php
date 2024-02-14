df15e00c1ced9b2ab8710e58bdbe101d98dbefed
vad23klev <vad23klev@gmail.com>
2024-02-14 10:07:02 +0300

    update codeigniter

--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\Comment;
 
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('welcome_message');
-	}
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
 }
